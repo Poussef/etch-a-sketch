f327b0f888c27ae72255fab5f50de39483f2629e
Youssef Tawil <yousseftawil321@gmail.com>
2024-01-24 15:56:43 -0800

    finished basic etch-a-sketch

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-var gridSize = parseInt(prompt("Enter a grid size (ex. 16x16):"));
-const n = gridSize * gridSize;
-console.log(gridSize);
-console.log(n);
 const sketchFrame = document.querySelector("#frame");
+const frameDimension = 960;
+var n = parseInt(prompt("Enter a grid size (ex. 16x16):"));
+const sketchSize = frameDimension / n;
+
+var color = "black";
+const eraser = document.querySelector(".eraser");
+const colorRed = document.querySelector(".red");
+const colorBlue = document.querySelector(".blue");
+const colorGreen = document.querySelector(".green");
+eraser.addEventListener("click", () => 
+    color = "white"); 
+colorRed.addEventListener("click", () => 
+    color = "red"); 
+colorBlue.addEventListener("click", () => 
+    color = "blue"); 
+colorGreen.addEventListener("click", () => 
+    color = "green"); 
 
 for (i = 0; i < n; i++) {
-    const sketchDiv = document.createElement("div");
-    sketchDiv.classList.add("sketchDiv");
-    sketchDiv.innerHTML = "Test";
-    sketchFrame.append(sketchDiv);
-}
+    const sketchRow = document.createElement("div"); 
+    sketchRow.classList.add("sketchRow");
+    sketchRow.style.height = `${sketchSize}px`;
+    for (j = 0; j < n; j++) {
+        const sketchDiv = document.createElement("div");
+        sketchDiv.classList.add("sketchDiv");
+        sketchDiv.style.cssText = 
+            `height: ${sketchSize}px;
+             width: ${sketchSize}px;
+        `;
+        sketchRow.append(sketchDiv);
+    };
+    sketchFrame.append(sketchRow);
+};
+
+const cDiv = document.querySelectorAll(".sketchDiv");
+cDiv.forEach(div => {
+    div.addEventListener('mouseover', () => 
+        div.style.backgroundColor = `${color}`)
+})
